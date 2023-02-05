7719c0579210aa834bdf34fb8b78adb9b5803267
Daniel Antoš <dan.antos@seznam.cz>
2023-02-06 00:20:25 +0100

    refactor: date picker

---
+++ app/src/main/java/com/example/nanotank/DatePickerFragment.java
@@ -0,0 +1,44 @@
+package com.example.nanotank;
+
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.app.DialogFragment;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Locale;
+
+@SuppressLint("ValidFragment")
+public class DatePickerFragment extends DialogFragment
+        implements DatePickerDialog.OnDateSetListener {
+
+    TextView mResultText;
+
+    public DatePickerFragment(TextView textView) {
+        mResultText = textView;
+    }
+
+
+    @Override
+    public DatePickerDialog onCreateDialog(Bundle savedInstanceState) {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog dialog = new DatePickerDialog(getActivity(), android.R.style.Theme_Holo_Light_Dialog_NoActionBar,
+                this, year,month,day);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        return dialog;
+    }
+    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+        month=month+1;
+
+        String date = String.format(Locale.GERMANY, "%d.%d.%d", day, month, year);
+        mResultText.setText(date);
+    }
+}
--- app/src/main/java/com/example/nanotank/MainActivity.java
+++ app/src/main/java/com/example/nanotank/MainActivity.java
@@ -14,8 +14,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -23,7 +21,6 @@ import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.NumberPicker;
 import android.widget.ProgressBar;
@@ -35,9 +32,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationManagerCompat;
 
 import com.example.nanotank.bluetooth.BluetoothConnection;
-
-import java.util.Calendar;
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -111,15 +105,8 @@ public class MainActivity extends AppCompatActivity {
         dateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Calendar cal = Calendar.getInstance();
-                int year = cal.get(Calendar.YEAR);
-                int month = cal.get(Calendar.MONTH);
-                int day = cal.get(Calendar.DAY_OF_MONTH);
-
-                DatePickerDialog dialog = new DatePickerDialog(
-                        MainActivity.this,android.R.style.Theme_Holo_Light_Dialog_NoActionBar,mDateSetListener,year,month,day);
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                dialog.show();
+                DialogFragment newFragment = new DatePickerFragment(textDate);
+                newFragment.show(getFragmentManager(), "datePicker");
             }
         });
 
@@ -162,17 +149,6 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
-
-        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                month=month+1;
-                Log.d(TAG, "onDateSet: dd/mm/yyyy:" + day + "/" + month + "/" + year);
-
-                String date = String.format(Locale.GERMANY, "%d.%d.%d", day, month, year);
-                textDate.setText(date);
-            }
-        };
         imgLedOn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
